94fd115b42356caf26edf43077e4e9227196c123
JMaio <10330349+JMaio@users.noreply.github.com>
2020-02-03 17:43:07 +0000

    update sender + receiver

--- dummynetshared/Receiver1a.java
+++ dummynetshared/Receiver1a.java
@@ -1,3 +1,97 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+
+/* Joao Maio s1621503 */
+
 // The receiver should store the transmitted data
 // (after removing header from packet) into a local file
 
+// Receiver program must be named as specified below and must accept the
+// following options from the command line:
+
+// java Receiver1a <Port> <Filename>
+//      <Port> is the port number which the receiver will use for receiving messages from the sender.
+//      <Filename> is the name to use for the received file to save on local disk.
+
+// For example: java Receiver1a 54321 rfile
+// Expected output: A successfully transferred file to the receiver; both sent and
+// received files must be identical at a binary level when checked using the “diff” command.
+
+/**
+ * Receiver1a
+ */
+public class Receiver1a {
+
+    static int port;
+    static UDPServer server;
+
+    static String filename;
+
+    static int dataPacketSize = 1024;
+
+    public static DatagramPacket waitAndGetInitial() throws IOException {
+        DatagramPacket p = null;
+        while (p == null) {
+
+            p = server.receivePacket();
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        return p;
+    }
+
+    public static void receiveFile(DatagramPacket initial) throws IOException {
+        File file = new File("D:/data/file.txt");
+        //create file if not exists
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        FileOutputStream fos = new FileOutputStream(filename);
+        
+        server.receivePacket();
+
+
+    }
+
+    public static void parseArgs(String[] args) throws Exception {
+        // Check how many arguments were passed in
+        if(args.length != 2) {
+            throw new Exception("incorrect usage - correct usage is:\njava Receiver1a <Port> <Filename>");
+        }
+        // attempt to convert arguments - exit if error
+        try {
+            port = Integer.parseInt(args[0]);
+            filename = args[1];
+            System.out.println(String.format("listening on port '%d' and writing to file '%s'", port, filename));
+        } catch (Exception e) {
+            throw new Exception("argument parse error:" + e);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            parseArgs(args);
+
+            server = new UDPServer(port);
+
+            DatagramPacket i = waitAndGetInitial();
+            System.out.println("initial packet received");
+
+            receiveFile(i);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+        
+    }
+
+    
+}
--- dummynetshared/Sender1a.java
+++ dummynetshared/Sender1a.java
@@ -25,49 +25,51 @@ public class Sender1a {
     static String remoteHost;
     static int port;
     
-    UDPClient client;
+    static UDPClient client;
 
     static String filename;
 
-    static int packetSize = 10;
-
+    static int dataPacketSize = 1024;
 
     public static void sendFile() throws Exception {
         
         FileInputStream fis = new FileInputStream(filename);
         int filesize = fis.available();
-        BufferedInputStream bis = new BufferedInputStream(fis, packetSize);
+        BufferedInputStream bis = new BufferedInputStream(fis, dataPacketSize);
         
         // https://stackoverflow.com/questions/1074228/is-there-any-java-function-or-util-class-which-does-rounding-this-way-func3-2
         // Divide x by n rounding up
         // int res = (x+n-1)/n
         
         // end of file packet = total size / packet size
-        byte eof = (byte) ((filesize + packetSize - 1) / packetSize);
+        byte eof = (byte) ((filesize + dataPacketSize - 1) / dataPacketSize);
         System.out.println("packets to transmit = " + (int) eof);
 
         System.out.println("--------");
         
-        for (int seq = 0; seq * packetSize < filesize; seq++) {
+        for (int seq = 0; seq * dataPacketSize < filesize; seq++) {
 
-            byte[] header = new byte[3];
-            // convert seq to 2 bytes
-            // https://stackoverflow.com/questions/1735840/how-do-i-split-an-integer-into-2-byte-binary
-            header[0] = (byte) (seq & 0xFF);
-            header[1] = (byte) ((seq >> 8) & 0xFF);
-            header[2] = eof;
+            // // header => | seq number (2Bytes) | last packet (1Byte) |
+            // byte[] header = new byte[3];
+            // // convert seq to 2 bytes
+            // // https://stackoverflow.com/questions/1735840/how-do-i-split-an-integer-into-2-byte-binary
+            // header[0] = (byte) (seq & 0xFF);
+            // header[1] = (byte) ((seq >> 8) & 0xFF);
+            // header[2] = eof;
 
             // this is the data in the packet
-            byte[] data = new byte[packetSize];
+            byte[] data = new byte[dataPacketSize];
             // buffer the input for the next packet
             bis.read(data);
+            
+            CustomUDPPacketData pkt = new CustomUDPPacketData(seq, eof, data);
+            // // concatenate header with data
+            // // https://stackoverflow.com/questions/5513152/easy-way-to-concatenate-two-byte-arrays
+            // byte[] pkt = new byte[header.length + data.length];
+            // System.arraycopy(header, 0, pkt, 0, header.length);
+            // System.arraycopy(data, 0, pkt, header.length, data.length);
 
-            // for (byte b : data) {
-            //     System.out.println(b);
-            // }
-
-            // TODO: send the data
-            client.sendPacket(data);
+            client.sendPacket(pkt.toByteArray());
             
             // In the sender code, insert, at a minimum, a 10ms gap (i.e., sleep for 10ms)
             // after each packet transmission.
@@ -104,7 +106,7 @@ public class Sender1a {
             parseArgs(args);
 
             client = new UDPClient(remoteHost, port);
-            // connect();
+            // packetSize = client.maxPacketSize;
 
             sendFile();
             
