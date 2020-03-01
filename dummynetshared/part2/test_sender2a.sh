6703ca7d297e7dfc2bd45024e638910813062ef4
Joao Maio <10330349+JMaio@users.noreply.github.com>
2020-03-01 21:38:48 +0000

    add config files

--- dummynetshared/part2/test_sender2a.sh
+++ dummynetshared/part2/test_sender2a.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # java Sender2a <RemoteHost> <Port> <Filename> <RetryTimeout> <WindowSize>
-clear; java Sender2a localhost 100 test.jpg 1000 1
+clear; java Sender2a localhost 100 test.jpg 20 2
